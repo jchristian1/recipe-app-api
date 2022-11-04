924ddc528d7e77255e81f2fdf5394abbccef6833
jchristian1 <jchristian_1@hotmail.com>
2022-11-03 22:20:19 -0400

    Fixing erros in authentication

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 
 from rest_framework.test import APIClient
 from rest_framework import status
+ME_URL = reverse('user:me')
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
@@ -16,6 +17,7 @@ def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
 
+"""Unauthenticated tests"""
 class PublicUserApiTest(TestCase):
     """Test the public features of the user API."""
 
@@ -109,3 +111,49 @@ class PublicUserApiTest(TestCase):
 
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test authentication is required for user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+"""Authenticated tests"""
+class PrivateUserApiTests(TestCase):
+    """Test API Request that require authentication"""
+    def setUp(self):
+        self.user = create_user(
+            email= 'test@example.com',
+            password= 'testpass123',
+            name= 'Test nmae'
+        )
+
+        self.client = APIClient()
+        self.client = force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data,{
+            'name':self.user.name,
+            'email': self.user.email,
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test POST is not allowed for the me endpoint."""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for the authenticated user."""
+        payload = {'name':'Updated name', 'paswword': 'nerpassword123'}
+
+        res = self.client.patch(ME_URL,payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['paswword']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
