2eec35389b77ff5be007f9004267c841a3f45d41
jchristian1 <jchristian_1@hotmail.com>
2022-11-08 14:16:07 -0500

    Implemeting deleting functionality

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -18,16 +18,16 @@ TAGS_URL = reverse('recipe:tag-list')
 
 def detail_url(tag_id):
     """Create a tag detail url."""
-    return reverse('recipe:tag-list')
+    return reverse('recipe:tag-detail', args=[tag_id])
 
 
-def create_user(email='user@example,com', password='testpas123'):
+def create_user(email='user@example.com', password='testpass123'):
     """Create and return a user"""
     return get_user_model().objects.create_user(email=email, password=password)
 
 
 class PublicTagsApiTests(TestCase):
-    """Test authenticated API requests."""
+    """Test unauthenticated API requests."""
 
     def setUp(self):
         self.client = APIClient()
@@ -39,7 +39,7 @@ class PublicTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class PrivateTagsApiTest(TestCase):
+class PrivateTagsApiTests(TestCase):
     """Test Authenticated API requests."""
 
     def setUp(self):
@@ -74,7 +74,7 @@ class PrivateTagsApiTest(TestCase):
 
     def test_update_tag(self):
         """Test updating a tag."""
-        tag = Tag.objects.create(user=self.user, name="After Dinner")
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
 
         payload = {'name': 'Dessert'}
         url = detail_url(tag.id)
@@ -83,3 +83,14 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         tag.refresh_from_db()
         self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test deleting tag."""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertEqual(tags.exists())
