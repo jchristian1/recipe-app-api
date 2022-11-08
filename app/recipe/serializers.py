d79e4a188d056fbbd51d5814c7185ff7e2ad19bc
jchristian1 <jchristian_1@hotmail.com>
2022-11-07 21:43:24 -0500

    Implementing Tags models.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -18,3 +18,10 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ['id','name']
+        read_only_fields = ['id']
